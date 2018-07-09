28fd5c50289f832b261ef37be48e1aa983d7bc45
LuckyAmy <15389663823@163.com>
2018-07-09 12:59:03 +0800

    pos系统

---
+++ vue-post/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <i class="icon iconfont icon-homepage"></i>
+    <i class="icon iconfont icon-browse"></i>
+    <i class="icon iconfont icon-service"></i>
+    <i class="icon iconfont icon-people"></i>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
